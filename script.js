b4f5ea9622dffd16f0670a59bddeb4d1fa209fd1
Roberts Ozols <roberts.ozols@svs.edu.lv>
2025-05-09 12:57:12 +0300

    nosaucu mainīgos. režģa lielumam un mīnu skaitam iegūstu vērtību no html input

---
+++ script.js
@@ -0,0 +1,10 @@
+let rezgaIzm = 5;
+let minuSk = 5;
+let minuVietas = new Set();
+let revealedSafe = 0;
+let totalSafe = 0;
+let gameOver = false;
+function saktSpeli() {
+    const rezgaIzm = parseInt(document.getElementById('gridSize').value);
+    const minuSk = parseInt(document.getElementById('bombCount').value);
+}
